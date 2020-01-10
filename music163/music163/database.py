7a1c8a61e2f055e7042984cad0f4023054325b8c
xuzifan-github <42669936+xuzifan-github@users.noreply.github.com>
2020-01-10 18:48:28 +0800

    Update database.py

--- music163/music163/database.py
+++ music163/music163/database.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import pymysql
 
 MYSQL_DB = '163music'
 MYSQL_USER = 'root'
-MYSQL_PASS = 'root'
+MYSQL_PASS = 'A123456'
 MYSQL_HOST = 'localhost'
+MYSQL_PORT = 50036
 
 connection = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER,
                              password=MYSQL_PASS, db=MYSQL_DB,
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-
-if __name__ == '__main__':
-    cursor = connection.cursor()
-    sql = 'SELECT music_id,title,artist FROM music WHERE music_id NOT IN \
-                (SELECT music_id FROM music_file) ORDER BY music_id DESC'
-    cursor.execute(sql)
-    movies = cursor.fetchall()
-    for i in movies:
-        print(i)
